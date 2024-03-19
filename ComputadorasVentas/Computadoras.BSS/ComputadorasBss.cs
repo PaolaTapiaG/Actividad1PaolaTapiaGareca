0239dd7364431069ad0d226bd278016e93e2e091
Paola Andrea Tapia Gareca <tj.paola.tapia.g@upds.net.bo>
2024-03-19 10:23:16 -0400

    Nuevo

---
+++ ComputadorasVentas/Computadoras.BSS/ComputadorasBss.cs
@@ -0,0 +1,20 @@
+﻿using Computadoras.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computadoras.BSS
+{
+    public class ComputadorasBss
+    {
+       ComputadoraDAL dal = new ComputadoraDAL();
+
+        public DataTable ListarComputadorasBss()
+        {
+            return dal.ListarComputadoraDAL();
+        }
+    }
+}
